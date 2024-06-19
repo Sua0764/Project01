bb633d0da15a2d9642be93b4b0ffdfb5c74caf6e
Sua0764 <imdnasuecho@gmail.com>
2024-06-19 17:07:50 +0900

    서브메뉴 ui

--- majorflow/src/main/resources/static/js/about.js
+++ majorflow/src/main/resources/static/js/about.js
@@ -27,6 +27,17 @@ document.querySelector(".teacherInfo").addEventListener("click", () => {
   document.querySelector(".singleTeacherBox").classList.remove("hidden");
 });
 
+document.querySelectorAll(".subMenu > div").forEach((div) => {
+  div.addEventListener("click", () => {
+    document
+      .querySelectorAll(".subMenu > div")
+      .forEach((item) => item.classList.remove("active"));
+
+    // 클릭된 div에 active 클래스 추가
+    div.classList.add("active");
+  });
+});
+
 function sessionCurrent() {
   axios
     .get("http://localhost:8080/user/current", { withCredentials: true })
